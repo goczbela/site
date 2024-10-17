2e310cf5719ecfb67c902acec7d0aa20b05b222c
Gócz Béla <33186915+goczbela@users.noreply.github.com>
2024-10-17 18:54:51 +0200

    HTML dev

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("hello world!")
+
+
+
+const navIcon = document.getElementById("navIcon");
+
+navIcon.addEventListener("click", (e) => {
+    console.log("bla");
+    navIcon.classList.toggle("open");
+});
+
+
